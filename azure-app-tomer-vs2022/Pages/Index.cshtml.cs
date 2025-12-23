ddd939a67402df94ab0668d0ba8081f994d6ba30
Tomersh <Tomersh@TOMER-LEGION>
2025-12-23 19:30:38 +0200

    Add project files.

---
+++ azure-app-tomer-vs2022/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_tomer_vs2022.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
